7c581ef3c1073cf5b30c4f6453fb56db5182ba3d
Marco <marco@gulinux.net>
2016-05-12 19:02:29 +0200

    Initial import

---
+++ src/indi_property.h
@@ -0,0 +1,44 @@
+/*
+    Copyright (C) 2016 Marco Gulino (marco AT gulinux.net)
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+*/
+
+#ifndef INDI_PROPERTIES_LIB_INDI_PROPERTY
+#define INDI_PROPERTIES_LIB_INDI_PROPERTY
+
+#include <string>
+#include <vector>
+
+namespace INDI {
+namespace Properties {
+  template<typename VectorProperty, typename SingleProperty>
+  class Property {
+  public:
+    std::string name() const { return m_name; }
+    std::string device() const { return m_device; }
+    std::string m_label() const { return m_label; }
+    std::string m_group() const { return m_group; }
+  private:
+    const std::string m_device;
+    const std::string m_name;
+    const std::string m_label;
+    const std::string m_group;
+    std::vector<SingleProperty> m_properties;
+  };
+}
+}
+
+#endif
